fb353e47687ebe29cc005cd4a670bfa3d6e0c5dc
JTShocks <jacobdreyer2002@gmail.com>
2025-05-17 14:14:38 -0400

    Updating Animation Logic

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,23 +9,41 @@ public partial class PlayerController : CharacterBody2D
     [Export] 
 	public int moveSpeed;
 
+	bool isMoving;
+
+	[Export]
+	AnimationTree animationTree;
+	AnimationNodeStateMachinePlayback animationMode;
+
+	
+
 	private bool isFacingLeft = false;
 
     public bool isSelected { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-    //This is the generic player controller for moving the player around and getting player input
+	//This is the generic player controller for moving the player around and getting player input
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
 	{
 		camera = GetNode<Camera2D>("PlayerCamera");
+		var animationMode = animationTree.Get("parameters/playback");
+		//animationTree = GetNode<AnimationTree>("AnimationTree");
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		GetInput();
+				isMoving = inputDirection != Vector2.Zero && Velocity.Length() != 0;
+		UpdateAnimations();
 
+
+
+
+
+		
 		//Will move this around to account for jumping, but should work okay.
 		//Vector2 moveVector = new();
 		//moveVector = inputDirection * moveSpeed* (float)delta;
@@ -33,52 +51,60 @@ public partial class PlayerController : CharacterBody2D
 
 		//MoveAndCollide(moveVector);
 
-		Vector2 mousePos = GetGlobalMousePosition();
-        camera.GlobalPosition = GlobalPosition.Lerp(mousePos, 0.15f);
+		//Vector2 mousePos = GetGlobalMousePosition();
+		//camera.GlobalPosition = GlobalPosition.Lerp(mousePos, 0.15f);
 
-		if(mousePos.X < GlobalPosition.X && !isFacingLeft)
-		{
-			//Player is looking left
-			isFacingLeft = true;
-			FlipPlayer();
-		}
-		else if(mousePos.X > GlobalPosition.X && isFacingLeft)
-		{
-			isFacingLeft = false;
-			FlipPlayer();
-		}
+
 
 		//If we read the primary click or the secondary click
 		//Use the primary weapon
 
 	}
 
-    public override void _PhysicsProcess(double delta)
-    {
+	public override void _PhysicsProcess(double delta)
+	{
 		Velocity = inputDirection.Normalized() * moveSpeed;
+
 		MoveAndSlide();
+
+
+		
+
 		//if(Input.IsActionJustPressed("Interact"))
 		//{
 		//	GD.Print("Interacted");
 		//}
-    }
+	}
 
     public void GetInput()
     {
         inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		
+		GD.Print(inputDirection.ToString());
     }
 
-	void FlipPlayer()
+	void UpdateAnimations()
 	{
-		ApplyScale(new Vector2(-1,1));
+			animationTree.Set("parameters/Walk/blend_position", 1);
+
+
+		if (isMoving)
+		{
+			GD.Print("Going to Walk");
+			animationTree.Set("parameters/Idle/blend_position", inputDirection.X);
+			stateMachinePlayback.Travel("Walk");
+		}
+		else
+		{
+			GD.Print("Going to Idle");
+			stateMachinePlayback.Travel("Idle");
+		}
 	}
 
     public bool Interact()
-    {
-        //throw new NotImplementedException();
+	{
+		//throw new NotImplementedException();
 		return true;
-    }
+	}
 
     //public void Interact(Interactor interactor)
    // {
